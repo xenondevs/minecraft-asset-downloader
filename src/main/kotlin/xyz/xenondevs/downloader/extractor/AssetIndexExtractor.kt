fd0d5ec29e9433822a6ecc483e9890c02f77a0c6
ByteZ1337 <0xbytez@gmail.com>
2022-12-27 23:32:06 +0100

    Retry after rate-limit

--- src/main/kotlin/xyz/xenondevs/downloader/extractor/AssetIndexExtractor.kt
+++ src/main/kotlin/xyz/xenondevs/downloader/extractor/AssetIndexExtractor.kt
@@ -5,6 +5,7 @@ import io.ktor.client.*
 import xyz.xenondevs.downloader.util.IOUtils
 import xyz.xenondevs.downloader.util.downloadBuffered
 import java.io.File
+import java.net.ConnectException
 import java.util.function.Predicate
 import java.util.logging.Logger
 
@@ -23,11 +24,15 @@ internal class AssetIndexExtractor(
     
     override suspend fun extract() {
         val objects = index.getAsJsonObject("objects")
-        val toDownload = objects.keySet().filter { name -> filters.all { it.test("assets/$name") } }
+        val toDownload = objects.keySet().filter { name -> filters.all { it.test("assets/$name") } }.toMutableList()
         val total = toDownload.size
         val logStep = if (total > 100) 20 else 5
         var count = 0
-        toDownload.forEach { name ->
+        var failCount = 0
+        while (toDownload.isNotEmpty()) {
+            if (failCount > 5) throw ConnectException("Failed to download asset index. Check your firewall settings ($DOWNLOAD_URL).")
+            
+            val name = toDownload[0]
             val hash = objects.get(name).asJsonObject.get("hash").asString
             val url = "$DOWNLOAD_URL${hash.take(2)}/$hash"
             val file = File(outputDirectory, name)
@@ -35,9 +40,18 @@ internal class AssetIndexExtractor(
             if (!file.canonicalPath.startsWith(canonicalOutput + File.separatorChar))
                 throw IllegalStateException("File $file is outside of output directory!")
             
-            if (!file.exists() || IOUtils.getFileHash(file, "SHA1") != hash)
-                httpClient.downloadBuffered(url, file, null, hash, "SHA1")
-            if (++count % logStep == 0 || count == total) logger?.info("Downloaded $count/$total needed assets from asset index.")
+            if (!file.exists() || IOUtils.getFileHash(file, "SHA1") != hash) {
+                try {
+                    httpClient.downloadBuffered(url, file, null, hash, "SHA1")
+                } catch (ex: ConnectException) { // Likely rate-limited, wait a bit and try again
+                    ++failCount
+                    Thread.sleep(30000)
+                    continue
+                }
+                failCount = 0
+                if (++count % logStep == 0 || count == total) println("Downloaded $count/$total needed assets from asset index.")
+            }
+            toDownload.removeAt(0)
         }
     }
 }
